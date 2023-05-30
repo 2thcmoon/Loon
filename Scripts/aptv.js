64a6bfd4260d68b6bb2bfc7c01ce0bc9a4766429
Alex <56881023+Alexzzz6@users.noreply.github.com>
2023-05-30 23:09:40 +0800

    Add files via upload

---
+++ Scripts/aptv.js
@@ -0,0 +1,4 @@
+http-response https://api.revenuecat.com/v1/(receipts%7Csubscribers)/* script-path=https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/APTV.js, requires-body=true, tag=APTV恢复内购
+
+[MITM]
+hostname = api.revenuecat.com
---
+++ Scripts/focos.js
@@ -0,0 +1,8 @@
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body focos.vip.js 
+
+
+[mitm]
+hostname = focos.oracle.bendingspoonsapps.com
+
+***********************************/
+var obj=JSON.parse($response.body);obj.me.active_subscriptions_ids=["com.focos.1w_t8_1w"],$done({body:JSON.stringify(obj)});
