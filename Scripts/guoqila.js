11e512f91bc66c6717bd40a1857720f1b950cd63
Alexzzz <1184063840@qq.com>
2023-07-22 13:25:04 +0800

    :fire:添加过期啦会员解锁

---
+++ Scripts/guoqila.js
@@ -0,0 +1,21 @@
+*/
+var responseData = {
+    "result": {
+      "data": {
+        "user_is_pro": true,
+        "group_is_pro": true,
+        "membership_type": 5,
+        "event": {
+          "valid": true,
+          "id": "636a0b84f70b896dbde88aa6",
+          "icon": "https://raw.githubusercontent.com/Yu9191/-/main/nv5.PNG",
+          "type": "member",
+          "name": "baby",
+          "desc": "Baby wish you happy every day"
+        }
+      },
+      "code": 1
+    }
+  };
+  
+  $done({ body: JSON.stringify(responseData) });
