a3cf092247cc7ef8d45d26d613fb26d597f5a1f3
Alexzzz <1184063840@qq.com>
2023-07-31 16:59:47 +0800

    notability

--- Scripts/notability.js
+++ Scripts/notability.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*************************************
 
 项目名称：Notability
@@ -46,52 +45,3 @@ ojbk = {
 };
 
 $done({body: JSON.stringify(ojbk)});
-=======
-/*************************************
-
-项目名称：Notability
-下载地址：https://t.cn/A6Cgjtei
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-
-**************************************
-
-[rewrite_local]
-^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://github.com/2thcmoon/Loon/raw/main/Scripts/notability.js
-
-[mitm]
-hostname = notability.com
-
-*************************************/
-
-
-var ojbk = JSON.parse($response.body);
-
-ojbk = {
-  "data": {
-    "processAppleReceipt": {
-      "error": 0,
-      "subscription": {
-        "productId": "com.gingerlabs.Notability.premium_subscription",
-        "originalTransactionId": "570001184068302",
-        "tier": "premium",
-        "refundedDate": null,
-        "refundedReason": null,
-        "isInBillingRetryPeriod": false,
-        "expirationDate": "2099-12-31T23:59:59.999Z",
-        "gracePeriodExpiresAt": null,
-        "overDeviceLimit": false,
-        "expirationIntent": "CUSTOMER_CANCELLED",
-        "__typename": "AppStoreSubscription",
-        "user": null,
-        "status": "canceled",
-        "originalPurchaseDate": "2022-12-31T23:59:59.999Z"
-      },
-      "__typename": "SubscriptionResult"
-    }
-  }
-};
-
-$done({body: JSON.stringify(ojbk)});
->>>>>>> 6e1eee49f036005028fbd72fc1d08803d4d6c053
