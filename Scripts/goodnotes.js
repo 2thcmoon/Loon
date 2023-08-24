c14fd4e308a288f68ba81dbea549f412ef98a508
Alex <56881023+2thcmoon@users.noreply.github.com>
2023-08-24 18:01:43 +0800

    Update goodnotes.js

--- Scripts/goodnotes.js
+++ Scripts/goodnotes.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/goodnotes6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://github.com/2thcmoon/Loon/edit/main/Scripts/goodnotes.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://github.com/2thcmoon/Loon/edit/main/Scripts/goodnotes.js
 
 [mitm]
 hostname = isi.csan.goodnotes.com
