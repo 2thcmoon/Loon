f8c6e535560c0b94015029a0e8f89e1eb1cff288
Alexzzz <1184063840@qq.com>
2023-12-16 21:08:07 +0800

    update

--- Scripts/soutu.js
+++ Scripts/soutu.js
@@ -1,32 +1,55 @@
 /*************************************
 
-项目功能：搜图神器 解锁VIP(先登录)
-下载地址：https://t.cn/A6ogWd6z
-脚本作者：obj
-电报频道：https://t.me/obj
+项目功能：搜图神器 (先登录账号)
+下载地址：https://t.cn/A6o9s1WB
+版本支持：1.7.3
+更新日期：2023-11-30
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 使用方法：请先登录账号，再开脚本。
 
 **************************************
 
 [rewrite_local]
-^http:\/\/wallpaper\.soutushenqi\.com\/api\/.+\/account\/token url script-response-body https://raw.githubusercontent.com/obj/script/main/Rewrite/soutu.js
+^https?:\/\/.*\.soutushenqi\.com\/(api\/.+\/account\/(token|info)|cykj_community|v\d\/home\/dialog) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/soutu.js
 
 [mitm]
-hostname = wallpaper.soutushenqi.com
+hostname = *.soutushenqi.com
 
 *************************************/
 
 
-var obj = JSON.parse($response.body);
+var body = $response.body;
+var chxm1023 = JSON.parse(body);
+const user = /account\/(token|info)/;
+const aicl = /cykj_community\/(config\/tools\/.+|ai_draw\/self.+)/;
+const tcad = /home\/dialog/;
 
-obj.data.vipPastDueTime = 4092599350000;
-obj.data.vipLabel = "高级用户";
-obj.data.vipLabelLevel = 4;
-obj.data.vipType = 1;
-obj.data.pcVipType = 1;
-obj.data.pcVipPastDueTime = 4092599349000;
-obj.data.vitalityVipPastDueTime = 4092599349000;
-obj.data.vitalityPcVipPastDueTime = 4092599349000;
+if (user.test($request.url)) {
+    chxm1023.data = {
+        ...chxm1023.data,
+        "vipPastDueTime": 4092599349,
+        "vipLabelLevel": 4,
+        "vipLabel": "Lv10元老捐赠会员",
+        "pcVipPastDueTime": 4092599349,
+        "vipType": 1024,
+        "isVirtual": 1,
+        "vitalityPcVipPastDueTime": 4092599349,
+        "pcVipType": 1024,
+        "vitalityVipPastDueTime": 4092599349
+    };
+}
 
-$done({ body: JSON.stringify(obj) });
+if (aicl.test($request.url)) {
+    body = body.replace(/\"surplus":\d+/g, '\"surplus":99');
+    body = body.replace(/\"total":\d+/g, '\"total":99');
+    body = body.replace(/\"size":\d+/g, '\"size":0');
+}
+
+if (tcad.test($request.url)) {
+    chxm1023 = {};
+}
+
+body = JSON.stringify(chxm1023)
+$done({ body });
